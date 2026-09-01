cb5c4c1b8a7166af4e3c6a6367ca3e844b5aad65
Amiri.tech <amiriissa2005@gmail.com>
2026-09-01 11:39:22 -0700

    Delete media.routes.js

--- media.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const storageController = require("../controllers/storage.controller");
-
-// GET /media/:filename — hakuna API Key wala Access Token
-router.get("/:filename", storageController.serveMedia);
-
-module.exports = router;
